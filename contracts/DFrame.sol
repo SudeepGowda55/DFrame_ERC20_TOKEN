f702d9067fa9d142c34005193730114df384f47f
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-11-20 21:13:46 +0530

    burn functionality added and mint function removed

--- contracts/DFrame.sol
+++ contracts/DFrame.sol
@@ -1,33 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts@4.8.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.8.0/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@4.8.0/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts@4.8.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DFrame is ERC20, ERC20Burnable, ERC20Snapshot, Ownable {
-constructor() ERC20("D Frame", "DFT") {
-_mint(msg.sender, 100000000 * 10 ** decimals());
-}
+    constructor() ERC20("D Frame", "DFT") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
 
-function snapshot() public onlyOwner {
-_snapshot();
-}
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
 
-function mint(address to, uint256 amount) public onlyOwner {
-_mint(to, amount);
-}
+    function burnTokens(uint256 amount) public onlyOwner {
+        _burnTokens(amount);
+    }
 
-function burnTokens(uint256 amount) public onlyOwner {
-_burnTokens(amount);
-}
-// The following functions are overrides required by Solidity.
-
-function _beforeTokenTransfer(address from, address to, uint256 amount)
-internal
-override(ERC20, ERC20Snapshot)
-{
-super._beforeTokenTransfer(from, to, amount);
-}
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
